dd19f77e1b9df06f2e9ea76d1cd0c1a039063ab7
nazianah <nazianah.umme@gmail.com>
2023-12-21 10:51:50 +0400

    add new componet

---
+++ src/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Books = (props) => {
+  return (
+    <>
+    <h1>Books</h1>
+     <h2>Books name {props.Books}!</h2>
+    </>
+ 
+  )
+}
+
+export default Books
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AboutMe from './AboutMe';
+import Books from './Books';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <AboutMe name="NAZIANAH"/> 
+    <Books name="surround by idoits"/> 
   </React.StrictMode>
 );
 
